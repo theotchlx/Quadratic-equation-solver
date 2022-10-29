bc9742d710c938137b6247fc400de079ae845547
Théo Tchilinguirian <60852187+Theo-Tchilinguirian@users.noreply.github.com>
2022-10-29 22:26:05 +0200

    Update quad_eq_resolver.py

--- quad_eq_resolver.py
+++ quad_eq_resolver.py
@@ -22,7 +22,7 @@ def solve(a, b, c):
 	Not a very dynamically written function. >:( (not that I got the time for it)
 	"""
 	delta = b**2 - 4*a*c
-	print(delta)
+	print("delta =", delta)
 
 	if delta < 0:
 		print("the equation has no real solution.")
@@ -30,7 +30,7 @@ def solve(a, b, c):
 		# 2 real solutions
 		x1 = (-1*b + sqrt(delta))/(2*a)
 		x2 = (-1*b - sqrt(delta))/(2*a)
-		print("The solutions for f(x) = 0 are", x1, " and", x2)
+		print("The solutions for f(x) = 0 are", x1, "and", x2)
 	else:
 		# 1 real solutions
 		x0 = sqrt(delta)/(2*a)
@@ -129,32 +129,32 @@ def main():
 
 			print("f(x) =", sign(a) + str(sqrt(a**2)) + "x²", sign(b) + " " + str(sqrt(b**2)) + "x", sign(c) + " " + str(sqrt(c**2)))
 			
-			choosing = True
-			while choosing:
-				print("""Choose an option:
-				1. Solve f(x) = 0
-				2. Get variation table
-				3. Get variation table
-				3. Stop
-				""")
-				try:
-					opt = int(input("Choose an option: "))
-					if opt == 1:
-						solve(a, b, c)
-					elif opt == 2:
-						sign_table(a, b, c)
-					elif opt == 3:
-						var_table(a, b, c)
-					elif opt == 3:
-						print("Bye.")
-					else :
-						print("Choose a number between 1 and 4")
-						continue
-					choosing = False
-					break
-				except:
-					print("Invalid choice")
+		choosing = True
+		while choosing:
+			print("""Choose an option:
+			1. Solve f(x) = 0
+			2. Get variation table
+			3. Get variation table
+			3. Stop
+			""")
+			try:
+				opt = int(input("Choose an option: "))
+				if opt == 1:
+					solve(a, b, c)
+				elif opt == 2:
+					sign_table(a, b, c)
+				elif opt == 3:
+					var_table(a, b, c)
+				elif opt == 3:
+					print("Bye.")
+				else :
+					print("Choose a number between 1 and 4")
 					continue
+				choosing = False
+				break
+			except:
+				print("Invalid choice")
+				continue
 
 
 # Main :
